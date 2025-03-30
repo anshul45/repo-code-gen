64e7149b8b8a302d37f6cf4dd4e13cee9cab38e0
Abhilasha <singhabhilasha4042@gmail.com>
2025-03-30 23:26:26 +0530

    removes python server

--- server/curie_agent/__init__.py
+++
@@ -1,3 +0,0 @@
-from .SimpleAgent import SimpleAgent
-
-__all__ = ['SimpleAgent', 'BaseAgent']
--- server/tools/__init__.py
+++
@@ -1,3 +0,0 @@
-from .get_files_with_description import get_files_with_description
-
-__all__ = ['get_files_with_description']
