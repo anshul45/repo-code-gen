71e9a5726ae149d8f044e9e1392a10d2eb1cdcde
Abhilasha <singhabhilasha4042@gmail.com>
2025-04-01 11:03:42 +0530

    fix header

--- web/src/app/layout.tsx
+++ web/src/app/layout.tsx
@@ -1,6 +1,5 @@
 import { Inter } from "next/font/google";
 import "./globals.css";
-import Header from "@/components/Header"
 import { Providers } from "@/components/providers";
 
 const inter = Inter({
@@ -19,11 +18,7 @@ export default function RootLayout({
         <Providers>
           {/* Main content */}
           <div className="flex-1 flex flex-col">
-            <div className="flex items-center bg-gradient-to-br from-indigo-50 via-white to-purple-50 border-b">
-              <div className="flex-1">
-                <Header />
-              </div>
-            </div>
+          
             {children}
           </div>
         </Providers>
