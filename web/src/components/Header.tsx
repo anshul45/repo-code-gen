b404e875e194d81497def7f0d0f53b0ae3f553ac
Abhilasha <singhabhilasha4042@gmail.com>
2025-04-01 11:04:40 +0530

    remove download button

--- web/src/components/Header.tsx
+++ web/src/components/Header.tsx
@@ -1,9 +1,6 @@
 "use client"
 import React from 'react'
 import { usePathname } from 'next/navigation'
-import { Download } from 'lucide-react'
-import { useFileStore } from '@/store/fileStore'
-import JSZip from 'jszip'
 
 interface FileContents {
   contents: string;
@@ -18,50 +15,6 @@ interface FileNode {
   directory?: DirectoryContents;
 }
 
-interface FileInfo {
-  filename: string;
-  content: string;
-}
-
-const downloadZip = async (files: FileInfo[]) => {
-  const zip = new JSZip();
-  
-  files.forEach(file => {
-    zip.file(file.filename, file.content);
-  });
-  
-  const blob = await zip.generateAsync({ type: "blob" });
-  const url = window.URL.createObjectURL(blob);
-  const a = document.createElement('a');
-  a.href = url;
-  a.download = "project.zip";
-  document.body.appendChild(a);
-  a.click();
-  window.URL.revokeObjectURL(url);
-  document.body.removeChild(a);
-};
-
-const downloadDirectory = (node: FileNode, basePath: string): FileInfo[] => {
-  if (node.file) {
-    return [{
-      filename: basePath,
-      content: node.file.contents
-    }];
-  }
-
-  if (node.directory) {
-    return Object.entries(node.directory).flatMap(([key, value]: [string, FileNode]): FileInfo[] => {
-      const newPath = basePath ? `${basePath}/${key}` : key;
-      return downloadDirectory(value, newPath);
-    });
-  }
-
-  return Object.entries(node).flatMap(([key, value]: [string, FileNode]): FileInfo[] => {
-    const newPath = basePath ? `${basePath}/${key}` : key;
-    return downloadDirectory(value, newPath);
-  });
-};
-
 const Header = () => {
     const pathname = usePathname()
   return (
@@ -70,18 +23,6 @@ const Header = () => {
         :
         <div className='text-sm ml-4'>Project Description</div>
         }
-        <button 
-          type="button"
-          className="flex items-center gap-2 px-3 py-1 rounded-md hover:bg-gray-100 transition-colors ml-auto"
-          onClick={async () => {
-            const { files } = useFileStore.getState();
-            const allFiles = downloadDirectory(files, "");
-            await downloadZip(allFiles);
-          }}
-        >
-          <Download size={18} />
-          <span className="text-sm">Download Project</span>
-        </button>
     </div>
   )
 }
